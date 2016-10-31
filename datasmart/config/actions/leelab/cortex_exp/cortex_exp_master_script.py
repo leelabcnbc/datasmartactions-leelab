6331ff52954963f43b151e75b0bc1be4becd6010
Yimeng Zhang <zym1010@gmail.com>
2016-10-31 12:17:10 -0400

    fix bug

--- datasmart/config/actions/leelab/cortex_exp/cortex_exp_master_script.py
+++ datasmart/config/actions/leelab/cortex_exp/cortex_exp_master_script.py
@@ -4,9 +4,9 @@ if __name__ == '__main__':
     messy_data_root_deploy = '/datasmart/leelab/raw_data_messy'
     clean_data_root_deploy = '/datasmart/leelab/raw_data'
 
-    messy_data_root_debug = '/Users/yimengzh/Desktop/datasmart_raw_data'
-    clean_data_root_debug = '/Users/yimengzh/Desktop/datasmart_raw_data_messy'
+    messy_data_root_debug = '/Users/yimengzh/Desktop/datasmart_raw_data_messy'
+    clean_data_root_debug = '/Users/yimengzh/Desktop/datasmart_raw_data'
 
     clean_data_site_url = 'sparrowhawk.cnbc.cmu.edu'
 
-    cortex_exp_master_wrapper(messy_data_root_deploy, clean_data_root_deploy, clean_data_site_url)
+    cortex_exp_master_wrapper(clean_data_root_deploy, messy_data_root_deploy, clean_data_site_url)
